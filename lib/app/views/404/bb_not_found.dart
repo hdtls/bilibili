9e1123df71fba48417efc2cea050a98f06ea5d8d
Junfeng Zhang <jeffzx15o@gmail.com>
2020-01-23 13:52:35 +0800

    Refactor 404 page.

--- lib/app/views/404/bb_not_found.dart
+++ lib/app/views/404/bb_not_found.dart
@@ -1,14 +1,45 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:bilibili/app/utils/bb_args.dart';
 
 class BBNotFoundView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(20.0),
-        child: Center(
-          child: Text("The page you are looking for is undefine."),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(
+          "404",
         ),
+      ),
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return SingleChildScrollView(
+            padding: defaultMargin,
+            physics: AlwaysScrollableScrollPhysics(),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/images/circle_notAllowed150x130.png",
+                      width: 150.0,
+                      height: 130.0,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text(
+                      "您访问的页面不见了/(ToT)/~~",
+                      style: Theme.of(context).textTheme.subtitle,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
