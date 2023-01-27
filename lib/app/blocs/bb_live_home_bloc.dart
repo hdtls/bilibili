445d1e26775d254dd5f04f847e6c2d91e154942b
Junfeng Zhang <jeffzx15o@gmail.com>
2023-01-27 23:09:16 +0800

    fix: error importing deps

--- lib/app/blocs/bb_live_home_bloc.dart
+++ lib/app/blocs/bb_live_home_bloc.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../api/bb_api.dart';
 
